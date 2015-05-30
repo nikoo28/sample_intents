df7b34751af2aa3bf0478aa51ca891cc827c9c4f
Nikhil Lohia <nikhil.lohia@mobileum.com>
2015-05-31 01:18:28 +0530

    Received the message in Bacon Activity

--- app/src/main/java/com/nikoo28/intentsample/Bacon.java
+++ app/src/main/java/com/nikoo28/intentsample/Bacon.java
@@ -1,22 +1,35 @@
 package com.nikoo28.intentsample;
 
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.TextView;
 
 
 public class Bacon extends ActionBarActivity {
+
+    private static TextView mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bacon);
+
+        mEditText = (TextView) findViewById(R.id.baconText);
+        Bundle appleData = getIntent().getExtras();
+        if (appleData == null)
+            mEditText.setText("I got Nothing");
+        else {
+            String appleMessage = appleData.getString("appleMessage");
+            mEditText.setText(appleMessage);
+        }
+
     }
 
-    public void startApple(View view){
+    public void startApple(View view) {
         Intent mIntent = new Intent(this, Apples.class);
         startActivity(mIntent);
     }
